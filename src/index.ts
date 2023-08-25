22ac5abca968236236c4a100b37e1d218443ef77
Jani Haakana <jani.k.haakana@solita.fi>
2023-08-25 09:20:35 +0300

    Create a simple cli interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+#!/usr/bin/env node
+import { program } from "commander";
+
+program
+  .option("-c, --cert <path>", "Certificate path", "cert.pem")
+  .option("-k, --key <path>", "Key path", "key.pem")
+  .option("-o, --output-file <path>", "Output file", "output.json")
+  .option("-C, --config <path>", "Config file", "config.json");
+
+program.parse();
